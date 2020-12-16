2cd7f6c94462c5307c76d2c277f23ecfafa47e8c
Nic Chan <channicoleelise@gmail.com>
2020-12-16 09:48:25 +0800

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+module.exports = function(config) {
+  // Passthrough copy
+  config.addPassthroughCopy('src/images');
+
+  // 404
+  config.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, browserSync) {
+        const content_404 = fs.readFileSync('dist/404/index.html');
+
+        browserSync.addMiddleware('*', (req, res) => {
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
+  });
+
+  return {
+    dir: {
+      includes: "components",
+      layouts: "components/layouts",
+      output: 'dist'
+    },
+    passthroughFileCopy: true,
+    templateFormats: ['njk', 'md'],
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+  };
+};
