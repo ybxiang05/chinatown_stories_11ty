aff69d265b564a270808ef5fa3d1379771ca0522
Nic Chan <channicoleelise@gmail.com>
2020-12-21 14:03:17 +0800

    Fix up sass watch task

--- .eleventy.js
+++ .eleventy.js
@@ -1,46 +1,49 @@
-const fs = require('fs');
-const util = require('util');
+const fs = require("fs");
+const util = require("util");
 
-module.exports = function(config) {
+module.exports = function (config) {
   // Passthrough copy
-  config.addPassthroughCopy('static');
+  config.addPassthroughCopy("static");
 
   // 404
   config.setBrowserSyncConfig({
     callbacks: {
-      ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('dist/404/index.html');
+      ready: function (err, browserSync) {
+        const content_404 = fs.readFileSync("dist/404/index.html");
 
-        browserSync.addMiddleware('*', (req, res) => {
+        browserSync.addMiddleware("*", (req, res) => {
           // Provides the 404 content without redirect.
           res.write(content_404);
           res.end();
         });
-      }
-    }
+      },
+    },
   });
 
   // Filters
-  config.addFilter('console', function (value) {
+  config.addFilter("console", function (value) {
     return util.inspect(value);
   });
 
   // Collections
-  config.addCollection("interviews", function (collection) {
-    return [
-      ...collection.getFilteredByTag("interviews")
-    ].sort(function (a, b) { return a.data.order - b.data.order; });
+  config.addCollection("content", function (collection) {
+    return [...collection.getFilteredByGlob("content/**/*.md")].sort(function (
+      a,
+      b
+    ) {
+      return a.data.order - b.data.order;
+    });
   });
 
   return {
     dir: {
       includes: "components",
       layouts: "components/layouts",
-      output: 'dist'
+      output: "dist",
     },
     passthroughFileCopy: true,
-    templateFormats: ['njk', 'md', 'js'],
-    htmlTemplateEngine: 'njk',
-    markdownTemplateEngine: 'njk',
+    templateFormats: ["njk", "md", "js"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
   };
 };
